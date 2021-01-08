d01de16e128a9da2842ba7329adc38abd19585f2
cjmurrph <chris@seasoft.com.au>
2021-01-09 07:12:06 +0800

    Deleted everything that's been moved to show-graph

--- src/main/fx/example_layouts.clj
+++
@@ -1,8 +0,0 @@
-(ns fx.example-layouts
-  (:require
-    [fx.layout-view :as layout]
-    [audience-republic.example-data :as example]))
-
-(defn x-3 []
-  (let [g example/nodes-graph]
-    (layout/show-graph g)))
